14b07c1d7dd018c535049daee331a1dda0be4f12
erwan.achat <erwan.achat@universite-paris-saclay.fr>
2026-07-24 16:10:41 +0200

    refactor(github username): updated github user in links

--- src/app/info/personal-info.ts
+++ src/app/info/personal-info.ts
@@ -23,7 +23,7 @@ développement logiciel pour concrétiser mes idées.
   `,
   title: 'AI Engineer',
   linkedIn: 'https://www.linkedin.com/in/erwan-achat-069311233/',
-  github: 'https://github.com/r1ha/portfolio',
+  github: 'https://github.com/erwan-achat/portfolio',
   location: 'Paris, France',
   toeicLink: 'https://www.etsglobal.org/global/en/digital-score-report/89BEC18A1168FCC1AB9A8F5E1D08770ECEEC5A70420BDF6BD2153EAAAAEF1BD7ckZhRzJpei9RWitCbEtwbzJ4V243a282UkpOV2ZuYTRDWXFkU2lnUXl1enlyakM4?utm_source=dsr&utm_medium=qr-code-click&utm_campaign=sharing_dsr',
   toeicScore: '955/990 (niveau C1)',
